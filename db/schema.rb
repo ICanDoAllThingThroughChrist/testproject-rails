b829f614120fbdae47421a535e88047472fd4919
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2017-10-06 15:01:14 -0500

    included frequencies attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006190212) do
+ActiveRecord::Schema.define(version: 20171006195602) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comments"
@@ -30,7 +30,19 @@ ActiveRecord::Schema.define(version: 20171006190212) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "frequency1s", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "frequency2", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "frequency2s", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
